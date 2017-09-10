6f3920559c9dbfe9addc3e5c3a82c53565d35c59
alejobastos <alejandrobastos1@gmail.com>
2017-09-10 07:55:31 -0500

    links en el muenu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule, Routes} from '@angular/router';
+import {IndexComponent} from './jugador/index/index.component';
+
+import {AddComponent as OrderAddComponent} from './orders/add/add.component';
+
+import{JugadorModule} from './jugador/jugador.module';
+import{OrdersModule} from './orders/orders.module';
+
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CurrentOptionComponent } from './current-option/current-option.component';
+
+const appRoutes: Routes = [
+{path: 'jugador/index', component: IndexComponent},
+{path: 'orders/add', component: OrderAddComponent},
+
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CurrentOptionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    JugadorModule,
+    OrdersModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
